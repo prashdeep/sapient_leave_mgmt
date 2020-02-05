b1464df5e823d6daf64337e8d8b61216f89531be
pradeep <pradeep.kumar44@gmail.com>
2020-02-05 16:14:46 +0530

    committing comparable

--- src/com/sapient/collections/ToStringDemo.java
+++ src/com/sapient/collections/ToStringDemo.java
@@ -2,35 +2,38 @@ package com.sapient.collections;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class ToStringDemo {
 
 	public static void main(String[] args) {
 		User user1 = new User(1, "Kishan");
-		User user2 = new User(1, "Kishan");
-		
+		User user2 = new User(2, "Vinay");
+		User user3 = new User(12, "Hari");
+			
 		//System.out.println(user1);
 		//System.out.println(user2);
 		
 	//	System.out.println(user1 == user2);
 		//System.out.println(user1.equals(user2));
 		
-		Set<User> set = new HashSet<>();
+		Set<User> set = new TreeSet<>();
 		set.add(user1);
 		set.add(user2);
+		set.add(user3);
 		
-		System.out.println("Contains user2 "+set.contains(user2));
-		System.out.println(" Is empty >> "+set.isEmpty());
-		set.clear();
-		System.out.println(" Is empty >> "+set.isEmpty());
+//		System.out.println("Contains user2 "+set.contains(user2));
+//		System.out.println(" Is empty >> "+set.isEmpty());
+//		set.clear();
+//		System.out.println(" Is empty >> "+set.isEmpty());
 		
 		
-		System.out.println(set.size());
+		System.out.println(set);
 		
 	}
 }
 
-class User {
+class User implements Comparable<User>{
 	
 	private int id;
 	private String name;
@@ -74,6 +77,13 @@ class User {
 		} else if (!name.equals(other.name))
 			return false;
 		return true;
+	}
+
+
+	@Override
+	public int compareTo(User user) {
+		
+		return user.name.compareTo(this.name);
 	}
 	
 	
