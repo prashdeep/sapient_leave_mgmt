d66b37a2057eaf1a5b5f6307e7fa3dad10802a78
pradeep <pradeep.kumar44@gmail.com>
2020-02-07 09:53:50 +0530

    committing the jdbc code

---
+++ src/com/sapient/jdbc/JdbcInsertDemo.java
@@ -0,0 +1,50 @@
+package com.sapient.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcInsertDemo {
+	
+	public static void main(String[] args) {
+		//step1 
+		try (
+				Connection connection = 
+				DriverManager.getConnection("jdbc:mysql://localhost:3306/qe_users", "root", "welcome");
+		//step 2
+		Statement statement = connection.createStatement();
+		
+		//step 3
+		//statement.execute("insert into users (name, dept, email) values ('vinay', 'PAYROLL', 'vinay@gmail.com')");
+		
+		//executeQuery
+			) {
+			
+			int updateCount = statement.executeUpdate("update users set name= 'harish kumar' where id= 1");
+			if(updateCount > 0) {
+				System.out.printf("Number of records updated :  %d %n", updateCount);
+			}
+			
+			ResultSet resultSet = statement.executeQuery("select * from users");
+
+			
+			while(resultSet.next()) {
+				
+				int empId = resultSet.getInt("id");
+				String empName = resultSet.getString("name");
+				String empDept = resultSet.getString("dept");
+				String empEmail = resultSet.getString("email");
+				
+				System.out.printf("Employee with id: %d, name: %s, department: %s and email %s %n",
+						empId, empName, empDept, empEmail);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
